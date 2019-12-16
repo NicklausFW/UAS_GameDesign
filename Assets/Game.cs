2d6c3635797242c028c5d4bf1b89eaafed3eb9db
daurelius <55678808+daurelius@users.noreply.github.com>
2019-12-16 23:47:54 +0700

    alien bisa gerak + tambah colider

---
+++ Assets/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public AppManager appManager;
+    public SpaceShip player;
+    //referensi ke UI
+    public Text scoreText;
+    public Text health;
+    public Text scoreFinal;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+        health.text = player.heath.ToString();
+        {
+            appManager.OnGameover();
+            scoreFinal.text = scoreText.text;
+            score = 0;
+            player.heath = 5;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawnner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnner : MonoBehaviour
+{
+    public GameObject allien;
+
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwSpawn <= 0)
+        {
+            int rand = Random.Range(0, 5);
+            
+            Instantiate(allien, transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime; 
+        }
+    }
+}
